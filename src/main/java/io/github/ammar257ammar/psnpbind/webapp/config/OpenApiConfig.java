84d27f22f58951a1d90ffcdbc1a0bd8b86a5c236
Ammar Ammar <ammar257ammar@gmail.com>
2021-03-12 15:12:08 +0100

    minor fix

--- src/main/java/io/github/ammar257ammar/psnpbind/webapp/config/OpenApiConfig.java
+++ src/main/java/io/github/ammar257ammar/psnpbind/webapp/config/OpenApiConfig.java
@@ -1,21 +1,42 @@
 package io.github.ammar257ammar.psnpbind.webapp.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {   
 	
     @Bean
     public OpenAPI customOpenAPI() {
+    	
+    	Server server = new Server();
+        server.setUrl("https://psnpbind.org");
+        
+        List<Server> servers = new ArrayList<Server>();
+        servers.add(server);
+        
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addHeaders("Access-Control-Allow-Origin", new Header()
+                        .description("version header")
+                        .required(true)
+                        .schema(new StringSchema())))
+                .servers(servers)
                 .info(new Info().title("PSnpBind API").description(
                         "Pocket Snps's effect on binding affinity (PSnpBind) Restful API (OpenAPI v3)")
                 		.version("1.0")
@@ -23,5 +44,19 @@ public class OpenApiConfig {
                 		.license(new License().name("CC-BY-SA 4.0").url("https://creativecommons.org/licenses/by-sa/4.0/"))
                 		.contact(new Contact().name("Ammar Ammar").url("http://orcid.org/0000-0002-8399-8990")));
     }
+    
+    @Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
     
 }
--- src/main/java/io/github/ammar257ammar/psnpbind/webapp/rest/ApiController.java
+++ src/main/java/io/github/ammar257ammar/psnpbind/webapp/rest/ApiController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @Tag(name = "contact", description = "the Contact API")
 public class ApiController {
